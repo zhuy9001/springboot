e1ff0cbc5272758417be347621e75f69ae43516f
zhuy9001 <zhuyang_277@163.com>
2017-10-05 21:05:57 +0800

    添加事物

--- src/main/java/com/pgl/demo/service/impl/StudentService.java
+++ src/main/java/com/pgl/demo/service/impl/StudentService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pgl.demo.dao.impl.StudentMapper;
 import com.pgl.demo.domain.Student;
@@ -14,14 +15,19 @@ public class StudentService {
 	@Autowired
 	private StudentMapper studentMapper;
 	
+	@Transactional
 	public int add(String sname,String sex,int age) {
-		return studentMapper.add(sname, sex, age);
+		int i=studentMapper.add(sname, sex, age);
+		int m=i/0;
+		return i;
 	}
 
+	@Transactional
 	public int update(String sname,String sex,int age,int sid) {
 		return studentMapper.update(sname, sex, age, sid);
 	}
 	
+	@Transactional
 	public int delete(int sid) {
 		return studentMapper.delete(sid);
 	}
