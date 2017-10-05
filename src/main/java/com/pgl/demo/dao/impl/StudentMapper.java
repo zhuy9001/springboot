62645b0f5ba287d8e11acced0a6cef6ad15e60da
zhuy9001 <zhuyang_277@163.com>
2017-10-05 14:22:14 +0800

    spring boot mybatis 注解配置

---
+++ src/main/java/com/pgl/demo/dao/impl/StudentMapper.java
@@ -0,0 +1,34 @@
+package com.pgl.demo.dao.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.pgl.demo.domain.Student;
+
+@Mapper
+public interface StudentMapper {
+	
+	@Insert("insert into student(sname,ssex,sage) values(#{name},#{sex},#{age})")
+	int add(@Param("name") String name,@Param("sex") String sex,@Param("age") int age);
+	
+	
+	@Update("update student set sname=#{name},sage=#{age},ssex=#{sex} where sid=#{sid}")
+	int update(@Param("name")String name,@Param("sex") String sex,@Param("age") int age,@Param("sid") int sid); 
+	
+	@Delete("Delete from student where sid=#{sid}")
+	int delete(@Param("sid") int sid);
+	
+	@Select("select sid,sname,sage,ssex from student ")
+	List<Student> getStudentList();
+	
+	@Select("select sid,sname,sage,ssex from student where sid=#{sid} ")
+	Student findStudentById(@Param("sid") int sid);
+	
+
+}
